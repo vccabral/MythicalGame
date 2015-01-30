59730da0d2ac1dec3064be749c6074fca75843e4
Victor Cabral <vccabral@gmail.com>
2015-01-30 18:32:13 -0500

    removing database folder entirely

--- database/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-
-class SettingsTestCase(TestCase):
-	def test_settings(self):
-		from django.conf import settings
-
-		self.assertEqual(settings.TEST_RUNNER, 'django_nose.NoseTestSuiteRunner')
